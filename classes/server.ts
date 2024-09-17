8aaffc041d9f1ef9aab59b99f20754a575519890
LUISHY1402 <106778831+LUISHY1402@users.noreply.github.com>
2024-09-17 15:08:33 -0400

    Mensaje descriptivo de los cambios

--- classes/server.ts
+++ classes/server.ts
@@ -19,7 +19,7 @@ export default class Server {
     // this.io = new SocketOIServer(this.httpServer);
     this.io = new SocketOIServer(this.httpServer, {
       cors: {
-        origin: "https://cliente-socket-unandes.vercel.app",
+        origin: "https://cliente-socket-eight.vercel.app",
         methods: ["GET", "POST"],
       },
     });
